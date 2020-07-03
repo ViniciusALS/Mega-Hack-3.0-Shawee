9afa1703ea6756db241ac4b9b2be3cb628495457
Vinicius <me@viniciusals.com>
2020-07-03 17:35:12 -0300

    Cria rota para pagina home

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 import Cervejas from './components/cervejas/index';
+import Home from './pages/Home'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Route component={Cervejas} path="/" exact />
+			<Route component={Home} path="/home" />
         </BrowserRouter>
     );
 }
