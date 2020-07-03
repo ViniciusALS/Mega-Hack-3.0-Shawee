e6bc322ff36717fe7600e0eeebe765bb98635698
Vinicius <me@viniciusals.com>
2020-07-03 20:33:05 -0300

    Atualiza props passadas para objeto CardBar

--- src/components/CardBar/index.tsx
+++ src/components/CardBar/index.tsx
@@ -4,17 +4,18 @@ import './styles.css';
 import NotaDoBar from '../NotaDoBar';
 
 type CardBarProps = {
-	nome: string,
+  nome: string,
+  imageURL: string,
 	nota: number,
 	distance: number,
 	disponibilidades: [ string, string ]
 }
 
-const CardBar: React.FC<CardBarProps> = ({ nome, nota, distance, disponibilidades}) => {
+const CardBar: React.FC<CardBarProps> = ({ nome, imageURL, nota, distance, disponibilidades}) => {
 	
 	return (
 		<div className="card-bar">
-			<img src="" alt='Foto do bar' />
+			<img src={imageURL} alt='Foto do bar' />
       <h3>{nome}</h3>
       <NotaDoBar nota={nota} />
       <span>{distance}m</span>
