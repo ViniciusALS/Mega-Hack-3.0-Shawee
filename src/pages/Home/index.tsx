79fdfc2ac3e5eaeef174420828de69a74b6fa341
Vinicius <me@viniciusals.com>
2020-07-04 13:50:24 -0300

    Adiciona secoes da pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -27,14 +27,23 @@ const Home = () => {
 
 	return (
     <>
-			<Cervejas selected={setItems} />
+      <h2>Cervejas para você</h2>
+      <Cervejas selected={setItems} />
+      
+      <h3>Bares perto de você</h3>
       <CardBar
         nome="Social Burguer"
         imageURL="https://imgur.com/f1afkzh.png"
         nota={3}
         distance={300}
-        disponibilidades={[{ "id": 1, "nome": "Segurança contra o COVID-19", "imageURL": "https://imgur.com/iaTXemX.png" },
-        { "id": 3, "nome": "Area de Jogos", "imageURL": "https://imgur.com/8nCFLw7.png" }]} />
+        disponibilidades={[
+          { "id": 1, "nome": "Segurança contra o COVID-19", "imageURL": "https://imgur.com/iaTXemX.png" },
+          { "id": 3, "nome": "Area de Jogos", "imageURL": "https://imgur.com/8nCFLw7.png" }
+        ]}
+      />
+      
+      <h3>O que vai rolar</h3>
+      <h3>Promoções</h3>
 		</>		
 	);
 }
