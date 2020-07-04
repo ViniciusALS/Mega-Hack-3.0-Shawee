3275b88015a4b7e6088c870d488ea87b2d17f0ec
CarlosGeorge <carlosgeorge248@gmail.com>
2020-07-04 20:59:28 -0300

    Implementação tela de cadastro
    Implementação tela de cadastro

---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import './styles.css';
+
+const Cadastro = () => {
+
+    return (
+        <div id="container">
+            <header>
+                <h1>Criar uma conta</h1>
+            </header>
+            <form>
+                <div className="cabecalho">
+                    <h1>Cadastro</h1>
+                </div>
+                <div className="group-fields">
+                    <div className="field">
+                        <input type="text" placeholder="Nome" />
+                    </div>
+                    <div className="field">
+                        <input type="email" placeholder="E-mail" />
+                    </div>
+                    <div className="field">
+                        <input type="text" placeholder="Data Nascimento" />
+                    </div>
+                    <div className="field">
+                        <input type="text" placeholder="CPF" />
+                    </div>
+                    <div className="field">
+                        <input type="password" placeholder="Senha" />
+                    </div>
+                    <div className="field">
+                        <input type="password" placeholder="Confirmar senha" />
+                    </div>
+                    <div className="field-checkbox">
+                        <input type="checkbox" name="check"  id="check"/>
+                        <label htmlFor="check">Concordo com  <span>Termos e Condições</span></label>
+                    </div>
+                </div>
+                <div className="confirm-botao">
+                    <a href="" className="botao">Fazer cadastro</a>
+                </div>
+            </form>
+            <footer>
+                <h3>Já tem login? <span>Fazer login</span></h3>
+            </footer>
+        </div>
+    );
+}
+
+export default Cadastro;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,14 @@ import { Route, BrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home'
 import Detail from './pages/Details/index';
+import Cadastro from './pages/Cadastro/index';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Route component={Home} path="/" exact />
             <Route component={Detail} path="/details/:id" />
+            <Route component={Cadastro} path="/cadastro" />
         </BrowserRouter>
     );
 }
