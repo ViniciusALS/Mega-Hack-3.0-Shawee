8cd243daedfc37a3a3b6bcde2182170f123d66e2
Vinicius <me@viniciusals.com>
2020-07-04 13:39:59 -0300

    Adiciona todos elementos de CardBar

--- src/components/CardBar/index.tsx
+++ src/components/CardBar/index.tsx
@@ -3,22 +3,39 @@ import './styles.css';
 
 import NotaDoBar from '../NotaDoBar';
 
+type disponibilidade = {
+	id: number,
+	nome: string,
+	imageURL: string
+}
+
 type CardBarProps = {
-  nome: string,
-  imageURL: string,
+	nome: string,
+	imageURL: string,
 	nota: number,
 	distance: number,
-	disponibilidades: number[]
+	disponibilidades: disponibilidade[]
 }
 
 const CardBar: React.FC<CardBarProps> = ({ nome, imageURL, nota, distance, disponibilidades}) => {
-	
+
 	return (
 		<div className="card-bar">
-			<img src={imageURL} alt='Foto do bar' />
-      <h3>{nome}</h3>
-      <NotaDoBar nota={nota} />
-      <span>{distance}m</span>
+			<img className='card-bar-img' src={imageURL} alt='Foto do bar' />
+			<h3>{nome}</h3>
+			<NotaDoBar nota={nota} />
+			<span>{distance}m</span>
+			<div>
+				{disponibilidades.map(disponibilidade => {
+					return (
+						<img
+							key={disponibilidade.id}
+							className='card-bar-disponibilidade-img'
+							src={disponibilidade.imageURL}
+							alt={disponibilidade.nome} />
+					);
+				})}
+			</div>
 		</div>
 	);
 }
