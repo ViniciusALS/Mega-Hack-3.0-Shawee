7f177e48832ebc69ed170398bec3267196c53d30
CarlosGeorge <carlosgeorge248@gmail.com>
2020-07-05 16:16:50 -0300

    Adicionando servidor json-server online
    Adicionando servidor json-server online

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3004'
+    baseURL: 'https://my-json-server.typicode.com/carlos-george/cervApp'
 });
 
 export default api;
