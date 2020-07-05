5b0c87fd964dab8a76c65dced2bbd805310aadc7
Vinicius <me@viniciusals.com>
2020-07-05 12:50:11 -0300

    Atualiza card-evento

--- src/components/card-evento/index.tsx
+++ src/components/card-evento/index.tsx
@@ -1,42 +1,30 @@
 import React from 'react';
 import './styles.css';
 
-import NotaDoBar from '../NotaDoBar';
-
-type disponibilidade = {
-	id: number,
-	nome: string,
-	imageURL: string
+type Bar = {
+  id: number,
+  nome: string,
+  distancia: string
 }
 
-type CardBarProps = {
+type Evento = {
+  id: number,
 	nome: string,
 	imageURL: string,
-	nota: number,
-	distance: string,
-	disponibilidades: disponibilidade[]
+  bar: Bar,
+	data: string,
+	hora: string
 }
 
-const CardEvento: React.FC<CardBarProps> = ({ nome, imageURL, nota, distance, disponibilidades}) => {
+const CardEvento: React.FC<Evento> = ({ id, nome, imageURL, bar, data, hora }) => {
 
 	return (
-		<div className="card-bar">
-			<img className='card-bar-img' src={imageURL} alt='Foto do bar' />
-			<span className='card-bar-titulo'>{nome}</span>
-			<NotaDoBar nota={nota} />
-			<div className='card-bar-distance-division'></div>
-			<span className='card-bar-distancia'>{distance}</span>
-			<div className='card-bar-disponibilidades'>
-				{disponibilidades.map(disponibilidade => {
-					return (
-						<img
-							key={disponibilidade.id}
-							className='card-bar-disponibilidade-img'
-							src={disponibilidade.imageURL}
-							alt={disponibilidade.nome} />
-					);
-				})}
-			</div>
+		<div className="card-evento">
+			<img className='card-evento-img' src={imageURL} alt='Foto do evento' />
+      <span className='card-evento-titulo'>{nome}</span>
+			<span className='card-evento-distancia'>{bar.nome}</span>
+			<div className='card-evento-distance-division'></div>
+			<span className='card-evento-distancia'>{bar.distancia}</span>
 		</div>
 	);
 }
